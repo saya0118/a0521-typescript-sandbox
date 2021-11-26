6f138ae78c31dcc9706dd0163bf928524a5ccf5c
Francois Polo <andasan@gmail.com>
2021-11-26 14:16:57 -0800

    added annotations/interfaces

--- EXE/exercise-basic/ts-basic.js
+++ EXE/exercise-basic/ts-basic.js
@@ -1,106 +1,81 @@
 // ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
 //   Exercise 1 – Basic Types
 // ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
-
 // Objectives: 
 // • Annotate primitive types, arrays, and 'any' types
 // • Identify when type checking happens
 // • Afterwards we will analyze transpiler output
-
-const ExerciseBasic() => {
+var ExerciseBasic = function () {
     // ======== Exercise 1.1 ========
     // Instructions:
     // • Hover over red squigglies to inspect the TS errors.
     // • Hover over variables to inspect their types.
     // • Fix the error on line 18 by changing the value of pi to the expected type.
-   
-    let pi = '3.14159';
-    let tau = pi * 2;
-    
-    console.log('[Exercise 1.1]', `${tau} is ${pi} times two.`);
-  
+    var pi = 3.14159;
+    var tau = pi * 2;
+    console.log('[Exercise 1.1]', tau + " is " + pi + " times two.");
     // ======== Exercise 1.2 ========
     // Instructions:
     // • Inspect type of `pie`
     // • Add an explicit type annotation to `pie`
     // • Try assigning invalid types, for fun
-  
-    let pie;
+    var pie;
     pie = 'blueberry';
-  
-    console.log('[Exercise 1.2]', `I like to eat ${pie}-flavored pie.`);
-  
+    console.log('[Exercise 1.2]', "I like to eat " + pie + "-flavored pie.");
     // ======== Exercise 1.3 ========
     // Instructions:
     // • Inspect the error, then fix it.
-  
-    let isMark: boolean;
-  
-    console.log('[Exercise 1.3]', `${isMark ? 'Oh, hi Mark' : 'Who are you?'}`);
-  
+    var isMark = false;
+    console.log('[Exercise 1.3]', "" + (isMark ? 'Oh, hi Mark' : 'Who are you?'));
     // ======== Exercise 1.4 ========
     // Instructions:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
-    const integer = 6;
-    const float = 6.66;
-    const hex = 0xf00d;
-    const binary = 0b1010011010;
-    const octal = 0o744;
-    const negZero = -0;
-    const actuallyNumber = NaN;
-    const largestNumber = Number.MAX_VALUE;
-    const mostBiglyNumber = Infinity;
-  
-    const members: any[] = [
-      integer,
-      float,
-      hex,
-      binary,
-      octal,
-      negZero,
-      actuallyNumber,
-      largestNumber,
-      mostBiglyNumber
+    var integer = 6;
+    var float = 6.66;
+    var hex = 0xf00d;
+    var binary = 666;
+    var octal = 484;
+    var negZero = -0;
+    var actuallyNumber = NaN;
+    var largestNumber = Number.MAX_VALUE;
+    var mostBiglyNumber = Infinity;
+    var members = [
+        integer,
+        float,
+        hex,
+        binary,
+        octal,
+        negZero,
+        actuallyNumber,
+        largestNumber,
+        mostBiglyNumber
     ];
-  
-    members[0] = '12345';
-  
+    members[0] = 12345;
     console.log('[Exercise 1.4]', members);
-  
     // ======== Exercise 1.5 ========
     // Instructions:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
-    const sequence = Array.from(Array(10).keys());
-    const animals = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-    const stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
-    const allMyArrays = [sequence, animals, stringsAndNumbers];
-  
+    var sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var animals = ['pangolin', 'aardvark', 'echidna', 'binturong'];
+    var stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
+    var allMyArrays = [sequence, animals, stringsAndNumbers];
+    // Array<Array<number|string>>
     console.log('Exercise 1.5', allMyArrays);
-  
     // ======== Exercise 1.6 ========
     // Goal:
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
-  
     // We want to represent an inventoryItem as a structure where
     // the first entry is the item name and the second is the quantity
-  
-    const inventoryItem = ['fidget wibbit', 11];
-  
+    var inventoryItem = ['fidget wibbit', 11];
     // later we destructure it
-    const [name, qty] = inventoryItem;
-  
-    const msg = addInventory(name, qty);
-  
+    var name = inventoryItem[0], qty = inventoryItem[1];
+    var msg = addInventory(name, qty);
     console.log('[Exercise 1.6]', msg);
-  
-    function addInventory(name: string, quantity: number): string {
-      return `Added ${quantity} ${name}s to inventory.`;
+    function addInventory(name, quantity) {
+        return "Added " + quantity + " " + name + "s to inventory.";
     }
-  }
-  
-  ExerciseBasic()
+};
+ExerciseBasic();
---
+++ EXE/exercise-interface/ts-interface.ts
@@ -0,0 +1,113 @@
+// ⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇⏇
+//   Exercise 2 – Interfaces
+// ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
+
+// Objectives:
+// • Demonstrate structural typing (duck typing)
+// • Create an interface and implement it on a class
+// • Differentiate type aliases from interfaces
+
+const Exercise2 = () => {
+    // ======== Exercise 2.1 ========
+    // Instructions:
+    // • Create an interface `CartItem` and replace the param's type with it
+    // • Make variantId optional
+  
+    interface CartItem { 
+      id: number; 
+      title: string; 
+      variantId?: number 
+    }
+  
+    function addToCart(item: CartItem) {
+      console.log('[Exercise 2.1]', `Adding "${item.title}" to cart.`)
+    }
+  
+    addToCart({ id: 1, title: 'Concrete shoes' })
+  
+    // ======== Exercise 2.2 ========
+    // Instructions:
+    // • Create and implement an interface on `Person` to ensure it always has accessible
+    //   `name` and `age` member properties.
+  
+    interface Person {
+      name: string;
+      age: number;
+    }
+  
+    class Person implements Person {
+      constructor(public name: string, public age: number) {}
+    }
+  
+    const jane = new Person('Jane', 31)
+  
+    console.log('[Exercise 2.2]', `${jane.name} is ${jane.age} years old.`)
+  
+    // ======== Exercise 2.3 ========
+    // Instructions:
+    // • Create an interface `Coords` that has numeric `latitude` and `longitude` properties.
+    // • Extend the existing interface `City` (without modifying it inline) by adding a
+    //   `coords` property of type `Coords`.
+    // • Fix whatever is wrong with `tampa`
+  
+    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    //--------------------Please do not edit from here--------------------
+    // [do not edit] (pretend this is coming from external `foo.d.ts` lib)
+    interface City {
+      name: string
+    }
+    //-----------------------[/do not edit]-------------------------------
+    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+  
+    interface Coords {
+      latitude: number;
+      longitude: number;
+    }
+  
+    // interface Place extends City{
+    //   coords: Coords
+    // }
+  
+    interface City{
+      coords: Coords
+    }
+  
+    const montreal = {
+      coords: {
+        latitude: 42.332,
+        longitude: -73.324,
+      },
+      name: 'Montréal',
+    }
+  
+    const tampa = {
+      coords: {
+        latitude: 27.9478,
+        longitude: -82.4584,
+      },
+      name: 'Tampa',
+    }
+  
+    // (string | number) // union
+    // (string & number) //intersection
+  
+    function getCityInfo(city: City) {
+      const coords = `(${city.coords.latitude.toFixed(
+        3
+      )}, ${city.coords.longitude.toFixed(3)})`
+      return `${city.name.toUpperCase()} is located at ${coords}.`
+    }
+  
+    console.log(
+      '[Exercise 2.3]',
+      `${getCityInfo(montreal)} \n\n ${getCityInfo(tampa)}`
+    )
+  
+  }
+  
+  Exercise2()
+  
